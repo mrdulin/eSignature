140613d84aabb0af8f722c7d859f2660a7c831b4
mrdulin <novaline.dulin@gmail.com>
2018-04-26 16:45:49 +0800

    add adobe-sign-sdk, send test.txt file using transientDocuments and createAgreement

--- packages/adobe-sign/src/router.js
+++ packages/adobe-sign/src/router.js
@@ -1,8 +1,13 @@
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const rp = require('request-promise');
+const AdobeSignSdk = require('adobe-sign-sdk');
+const fs = require('fs');
+const path = require('path');
 
 const config = require('./config');
+
+const context = new AdobeSignSdk.Context();
 
 function router(app) {
   app.use(
@@ -25,16 +30,23 @@ function router(app) {
       res.send(`
         <p>Successfully authenticated</p>
         <p>Your oauth access_token: ${req.session.access_token}</p>
-        <a href="/user/me">获取用户信息</a>
+        <ul>
+          <li><a href="/user/me">user info</a></li>
+          <li><a href="/refreshToken">refresh token</a></li>
+          <li><a href="/logout">logout</a></li>
+          <li><a href="/transientDocuments">transientDocuments</a></li>
+        </ul>
       `);
     }
   });
 
   // https://secure.na2.echosign.com/public/oauth?redirect_uri=https://www.github.com/mrdulin&response_type=code&client_id=CBJCHBCAABAA9rzvVcanKyZcc1eMlnH3NSk2OmMXNZkT&scope=agreement_send
   app.get('/oauth', (req, res) => {
+    const scopes = ['user_login', 'user_read', 'agreement_write', 'agreement_send'];
+    const scope = scopes.join('+');
     const url = `${config.adobe_sign.auth_request_url}?redirect_uri=${
       config.adobe_sign.callback_url
-    }&response_type=code&client_id=${config.adobe_sign.client_id}&scope=user_login+user_read`;
+    }&response_type=code&client_id=${config.adobe_sign.client_id}&scope=${scope}`;
     console.log(url);
     res.redirect(url);
   });
@@ -86,6 +98,135 @@ function router(app) {
       .catch(err => {
         console.log('获取user信息失败');
         console.error(err);
+      });
+  });
+
+  app.get('/refreshToken', (req, res) => {
+    const { api_access_point, refresh_token } = req.session;
+    const grant_type = 'refresh_token';
+    const url = `${api_access_point}oauth/refresh`;
+    rp
+      .post(url, {
+        json: true,
+        form: {
+          refresh_token,
+          client_id: config.adobe_sign.client_id,
+          client_secret: config.adobe_sign.client_secret,
+          grant_type
+        }
+      })
+      .then(json => {
+        console.log('refresh token成功');
+        req.session.access_token = json.access_token;
+        req.expires_in = json.expires_in;
+        res.redirect('/');
+      })
+      .catch(err => {
+        console.log('refresh token失败');
+        console.error(err);
+      });
+  });
+
+  app.get('/logout', (req, res) => {
+    const { api_access_point, access_token } = req.session;
+    const url = `${api_access_point}oauth/revoke`;
+    rp
+      .post(url, {
+        resolveWithFullResponse: true,
+        form: {
+          token: access_token
+        }
+      })
+      .then(response => {
+        if (response.statusCode === 200) {
+          console.log('revoke token成功');
+          req.session.destroy(err => {
+            if (err) return console.error(err);
+            return res.redirect('/');
+          });
+        } else {
+          console.log(response);
+          res.redirect('/');
+        }
+      })
+      .catch(err => {
+        console.log('revoke token失败');
+        console.error(err);
+      });
+  });
+
+  app.get('/transientDocuments', (req, res) => {
+    const { access_token } = req.session;
+    const headerParams = { accessToken: access_token };
+    const filePath = path.resolve(__dirname, '../public/test.txt');
+    const fileBytes = fs.readFileSync(filePath);
+    const buffer = Buffer.from(fileBytes);
+    const transientDocumentsApi = new AdobeSignSdk.TransientDocumentsApi(context);
+
+    const opts = {};
+    opts.mimeType = 'text/plain';
+
+    transientDocumentsApi
+      .createTransientDocument(headerParams, 'test.txt', buffer, opts)
+      .then(transientDocumentCreationResponse => {
+        const transientDocumentId = transientDocumentCreationResponse.getTransientDocumentId();
+        console.log(`The Transient Id is ${transientDocumentId}`);
+        return transientDocumentId;
+      })
+      .then(transientDocumentId => {
+        const agreementsApi = new AdobeSignSdk.AgreementsApi(context);
+        const agreementsModel = AdobeSignSdk.AgreementsModel;
+
+        const recipientSetMemberInfos = [];
+        const userEmails = ['novaline@qq.com'];
+
+        for (let i = 0; i < userEmails.length; i += 1) {
+          const recipientInfo = new agreementsModel.RecipientInfo();
+          recipientInfo.setEmail(userEmails[i]);
+          recipientSetMemberInfos.push(recipientInfo);
+        }
+
+        const recipientSetInfo = new agreementsModel.RecipientSetInfo();
+        recipientSetInfo.setRecipientSetMemberInfos(recipientSetMemberInfos);
+        recipientSetInfo.setRecipientSetRole(agreementsModel.RecipientSetInfo.RecipientSetRoleEnum.SIGNER);
+        recipientSetInfo.setRecipientSetName('fjenning@adobe.com');
+
+        const recipientSetInfos = [];
+        recipientSetInfos.push(recipientSetInfo);
+
+        const fileInfo = new agreementsModel.FileInfo();
+        fileInfo.setTransientDocumentId(transientDocumentId);
+
+        const fileInfos = [];
+        fileInfos.push(fileInfo);
+
+        const documentCreationInfo = new agreementsModel.DocumentCreationInfo();
+        documentCreationInfo.setName('MyAgreement');
+        documentCreationInfo.setFileInfos(fileInfos);
+        documentCreationInfo.setRecipientSetInfos(recipientSetInfos);
+        documentCreationInfo.setSignatureType(agreementsModel.DocumentCreationInfo.SignatureTypeEnum.ESIGN);
+        documentCreationInfo.setSignatureFlow(
+          agreementsModel.DocumentCreationInfo.SignatureFlowEnum.SENDER_SIGNATURE_NOT_REQUIRED
+        );
+
+        const agreementCreationInfo = new agreementsModel.AgreementCreationInfo();
+        agreementCreationInfo.setDocumentCreationInfo(documentCreationInfo);
+
+        agreementsApi
+          .createAgreement(headerParams, agreementCreationInfo)
+          .then(agreementCreationResponse => {
+            const agreementId = agreementCreationResponse.getAgreementId();
+            console.log(`Agreement created for Id ${agreementId}`);
+            res.json({ agreementId });
+          })
+          .catch(apiError => {
+            console.log('createAgreement failed');
+            console.log(apiError);
+          });
+      })
+      .catch(apiError => {
+        console.log('上传失败');
+        console.error(apiError);
       });
   });
 }
